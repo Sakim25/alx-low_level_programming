4ee4a055c0a21f37e025165ecbed35656afcd500
Sakim25 <sakimn84@gmail.com>
2022-06-30 15:10:45 -0700

    Q5

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include<Madd stuff.h>
+/**
+*main - i have to go
+*Description: Doing my best
+*this program prints "Programming is like building a multilingual puzzle
+* Return: 0
+*/
+ int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
